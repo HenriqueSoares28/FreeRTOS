841738372a6a2b0735399bac146c39e3c4690ce1
Henrique França Carvalho Soares <henrique.soares.2811@gmail.com>
2024-09-17 16:18:14 -0300

    :sparkles: feat(tarefas): Adição de controle de suspensão e exclusão da task1

--- src/main.cpp
+++ src/main.cpp
@@ -3,6 +3,8 @@
 int cont1 = 0;
 int cont2 = 0;
 
+TaskHandle_t task1Handle = NULL;
+
 void task1(void *parameters) {
   for (;;) {
     Serial.print("Task 1: ");
@@ -23,14 +25,20 @@ void task2(void *parameters) {
   }
 }
 
+void importantTask(void *parameters) {
+    vTaskSuspendAll(); // Suspende todas as tarefas
+    Serial.println("Task 3");
+    xTaskResumeAll(); // Resume todas as tarefas
+}
+
 void setup() {
   Serial.begin(9600);
-  xTaskCreate(task1,    // function name
-              "Task 1", // task name
-              1000,     // stack size
-              NULL,     // parameters
-              1,        // priority
-              NULL      // task handle
+  xTaskCreate(task1,       // function name
+              "Task 1",    // task name
+              1000,        // stack size
+              NULL,        // parameters
+              1,           // priority
+              &task1Handle // task handle
   );
 
   xTaskCreate(task2,    // function name
@@ -42,4 +50,15 @@ void setup() {
   );
 }
 
-void loop() {}
+void loop() {
+    if (cont1 == 3 && task1Handle != NULL) { // Verifica se task1Handle é diferente de NULL para evitar que o Loop seja suspendido
+        vTaskSuspend(task1Handle);
+    }
+    if (cont2 == 5 && task1Handle != NULL) { 
+        vTaskResume(task1Handle);
+    }
+    if (cont1 == 10 && task1Handle != NULL) {
+        vTaskDelete(task1Handle);
+        task1Handle = NULL;
+    }
+}
